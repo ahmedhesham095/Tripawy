10287d816b7d348ff94738d945fb9c4a01487d51
Aya <ayahilal16@gmail.com>
2018-04-27 09:22:03 +0200

    edit1

--- app/src/main/java/com/example/delllaptop/projone/AddTripActivity.java
+++ app/src/main/java/com/example/delllaptop/projone/AddTripActivity.java
@@ -29,6 +29,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+
 import com.example.delllaptop.projone.DTO.Trip;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
